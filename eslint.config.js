a445bb8d798c4544b8db695b0636ca66b7a01189
aidlran <86127892+aidlran@users.noreply.github.com>
2024-10-22 21:47:24 +0100

    update dependencies

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+// @ts-expect-error
+import eslint from '@eslint/js';
+// @ts-expect-error
+import prettier from 'eslint-config-prettier';
+import svelte from 'eslint-plugin-svelte';
+import ts from 'typescript-eslint';
+
+export default ts.config(
+  eslint.configs.recommended,
+  ...ts.configs.strict,
+  ...svelte.configs['flat/recommended'],
+  prettier,
+  ...svelte.configs['flat/prettier'],
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser,
+      },
+    },
+  },
+  {
+    ignores: ['.svelte-kit/', 'build/', 'lib/', 'static/docs'],
+  },
+  {
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'off',
+      'no-console': 'error',
+      'no-debugger': 'error',
+    },
+  },
+);
